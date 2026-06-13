c9f279bd8cae46c9fcb9ebf8d2425e3f7580ed3c
Johnny Belmonte <jb32225@gmail.com>
2026-06-12 21:08:49 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Welcome to PokeApp</h1>
+        <h2>Discover and explore the world of Pokémon with our comprehensive app. Whether you're a seasoned trainer or just starting your journey, PokeApp has everything you need to beat 'em all!</h2>
+          <div className="Search">
+            <input type="text" placeholder="Search for a Pokémon..." />
+            <button>Search</button>
+          </div>
+    </div>
+  )
+  
+}
+
+export default App
